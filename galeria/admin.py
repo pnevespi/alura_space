4f51baa3f7544295f2c31df96e662f6fad7951e8
Paulo Neves <pnevespi@gmail.com>
2026-06-29 23:36:14 -0300

    Adicionado filtros, novos campos no modelo e paginação no admin

--- galeria/admin.py
+++ galeria/admin.py
@@ -5,9 +5,12 @@ from galeria.models import Fotografia
 
 
 class ListandoFotografias(admin.ModelAdmin):
-    list_display = ("id", "nome", "legenda")
+    list_display = ("id", "nome", "legenda", "publicada")
     list_display_links = ("id", "nome")
     search_fields = ("nome",)
+    list_filter = ("categoria",)
+    list_editable = ("publicada",)
+    list_per_page = 1
 
 admin.site.register(Fotografia, ListandoFotografias)
 
