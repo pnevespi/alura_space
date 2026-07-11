696e277bde63e7e3b779590952fdd8afed51b89f
Paulo Neves <pnevespi@gmail.com>
2026-07-11 15:06:38 -0300

    Implementação do cadastro de usuário, login e logout

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -23,3 +23,53 @@ class LoginForms(forms.Form):
             }
         )
     )
+
+class CadastroForm(forms.Form):
+    nome_cadastro = forms.CharField(
+        label="Nome de Cadastro",
+        required=True,
+        max_length=100,
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control",
+                "placeholder":"Ex: João Silva"
+            }
+        )
+    )
+
+    email = forms.EmailField(
+        label="E-mail",
+        required=True,
+        max_length=100,
+        widget=forms.EmailInput(
+            attrs={
+                "class": "form-control",
+                "placeholder":"Ex: joao.silva@email.com"
+            }
+        )
+
+    )
+
+    senha_1 = forms.CharField(
+        label  = "Senha",
+        required=True,
+        max_length=70,
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "form-control",
+                "placehoder": "Digite sua senha."
+            }
+        )
+    )
+
+    senha_2 = forms.CharField(
+        label  = "Confirme sua Senha",
+        required=True,
+        max_length=70,
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "form-control",
+                "placehoder": "Confirme sua senha."
+            }
+        )
+    )
