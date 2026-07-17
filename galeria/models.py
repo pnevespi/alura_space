2bd78acbc02381bffe9b4148fd26024c1f38e359
Paulo Neves <pnevespi@gmail.com>
2026-07-16 21:04:48 -0300

    Tratamento de campos nome do usuário com espaço e senhas iguais (senha 1 e 2)

--- galeria/models.py
+++ galeria/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from datetime import datetime
+from django.contrib.auth.models import User
+
 
 # Create your models here.
 
@@ -18,6 +20,14 @@ class Fotografia(models.Model):
     foto = models.ImageField(upload_to="fotos/%Y/%m/%d",blank=True)
     publicada = models.BooleanField(default=False)
     data_fotografia = models.DateTimeField(default=datetime.now, blank=False)
+    usuario = models.ForeignKey(
+        to=User,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=False,
+        related_name="user",
+    )
+
 
     def __str__(self):
         return f"Fotografia [nome={self.nome}]"
--- usuarios/forms.py
+++ usuarios/forms.py
@@ -73,3 +73,22 @@ class CadastroForm(forms.Form):
             }
         )
     )
+
+    def clean_nome_cadastro(self):
+        nome = self.cleaned_data.get("nome_cadatro")
+        if nome:
+            if " " in nome:
+                raise forms.ValidationError("Não é possível inserir espaços dentro do campo nome de cadastro!")
+            else:
+                return nome
+
+    def clean_senha_2(self):
+        senha_1 = self.cleaned_data.get("senha_1")
+        senha_2 = self.cleaned_data.get("senha_2")
+
+        if senha_1 and senha_2:
+            if senha_1!=senha_2:
+                raise forms.ValidationError("Senhas não são iguais")
+            else:
+                return senha_2
+
